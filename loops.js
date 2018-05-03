783a6aa115e6972a03cb530b55dea4c4d3a3c50b
Rebecca Govert <becci.govert@gmail.com>
2018-05-03 20:42:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(givenArray) {
   var strangeArray = [];
   var count = 0;
-  for(var i=1;i<givenArray.length;i++) {
+  for(var i=1;i<26;i++) {
     if (i === 1) {
       strangeArray.push("I am 1 strange loop.");
     }
-    if (i>1 && i<26) {
+    if (i>1) {
       strangeArray.push(`I am ${i} strange loops.`);
       count++;
   }

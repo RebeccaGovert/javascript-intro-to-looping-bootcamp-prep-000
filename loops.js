ac0b8cf39c9de2257e8b2e1c2b02de026a1ef936
Rebecca Govert <becci.govert@gmail.com>
2018-05-03 19:41:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,13 @@
 function forLoop(array) {
   var resultsArray= [];
   var count = 0;
-  var myOutputLine = '';
+  var myOutputLine = `I am ${i} strange loop`;
   for (let i=0; i<array.length; i++) {
     if (i === 1) {
-      myOutputLine += `I am ${i} strange loop
-    }
+      myOutputLine += `.`
+      
+    } else if (i[count]>1, i[count]<26) {
+      newOutputline = myOutputLine += `s.`;
   }
+  return resultsArray.push(myOutputLine) + (newOutputline);
 }

68779bc2681f63b8acfc08c18b66e85bffbabe9a
Rebecca Govert <becci.govert@gmail.com>
2018-05-03 19:22:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,14 @@
 function forLoop(array) {
+  var results ='';
   var count = 0;
-  for ()
+  for (let i = 0; i < 25; i++) {
+   if (i === 1){
+    results.push('I am ' + array[i] + ' strange loop.');
+     
+   } else {
+     results.push()
+   }
+   }
+   
+  }
 }
